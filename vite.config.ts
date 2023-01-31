703d3119241285999a58c5185954dedf7a7f8d43
Mark <super16mail@gmail.com>
2023-01-31 12:51:07 +0300

    Remove trailing slash for base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/eesti/',
+  base: '/eesti',
   plugins: [vue()],
   resolve:{
     alias:{
